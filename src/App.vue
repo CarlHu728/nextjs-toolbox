35e2373ad4422c5d4bb169e2d1aa56ca54f99179
Chengcheng Hu <Chengchenghu0728@gmail.com>
2023-11-29 14:24:18 +0800

    update ui

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
 export default {
   methods: {
     playAudio() {
-      this.$refs.audio.play()
+      const audio = new Audio('test.wav');
+      audio.play()
     }
   }
 };
@@ -10,42 +11,76 @@ export default {
 </script>
 
 <template>
-  <div class="box">
+  <div class="background">
     <h1 class="title">Santa Hatter</h1>
-    <!-- <img src="/img.jpg"> -->
     <audio src="music2.ogg" autoplay loop></audio>
-    <audio ref="audio" src="test.wav"></audio>
-    <br>
-    <button class="button" @click="playAudio"></button>
+
+    <div class="empty-space"></div>
+
+    <div class="box">
+      <button class="button" @click="playAudio">
+        Click Me
+      </button>
+    </div>
   </div>
 </template>
 
 <style>
 .title {
   text-align: center;
-  font-size: 100px;
+  margin-top: 20px;
+  font-size: 200px;
   color: white;
 }
 
 .button {
-  text-align: center;
+  /* text-align: center; */
+  font-size: 100px;
+  color: white;
 
-  background-image: url('/img.jpg');
+  background-image: url('/button_normal.png');
+  background-color: rgba(0, 0, 0, 0);
   background-size: cover;
-  width: 50vw;
-  height: 50vh;
+
+  width: 400pt;
+  height: 160pt;
+
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+  /* 正常状态背景 */
+}
+
+/* 悬停状态 */
+.button:hover {
+  background-image: url('/button_hover.png');
+}
+
+/* 点击状态 */
+.button:active {
+  background-image: url('/button_pressed.png');
 }
 
 .box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: auto;
+}
+
+.background {
   position: absolute;
-  top: 0;
   left: 0;
+  top: 0;
   background-image: url('/background.webp');
   background-size: cover;
   background-position: center;
   height: 100vh;
   width: 100vw;
-  
+}
+
+.empty-space {
+  margin-bottom: 30vh;
 }
 
 </style>
