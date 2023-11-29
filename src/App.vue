2baa022133b8fec94945289e94f1325a3b7e38ed
Chengcheng Hu <Chengchenghu0728@gmail.com>
2023-11-29 13:24:37 +0800

    haha

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,36 @@
-<script setup>
-import { ref } from 'vue'
+<script>
+export default {
+  methods: {
+    playAudio() {
+      this.$refs.audio.play()
+    }
+  }
+};
 
-const msg = ref('Hello World!')
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-  <input v-model="msg">
+  <h1 class="title">色图</h1>
+
+  <br>
+  <!-- <img src="/img.jpg"> -->
+  <!-- <audio src="music.mp3" autoplay></audio> -->
+  <audio ref="audio" src="test.wav"></audio>
+  <button class="button" @click="playAudio"></button>
+
+  <br>
+
 </template>
+
+<style>
+.title {
+  text-align: center;
+  font-size: 100px;
+}
+.button {
+  background-image: url('/img.jpg');
+  background-size: cover;
+  width: 2000px;
+  height: 3000px;
+}
+</style>
